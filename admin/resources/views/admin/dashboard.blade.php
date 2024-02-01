d3887e6fcdc796f45d8419316af56763079b74a2
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-02-01 17:35:43 +0200

    started designing the nav-bar

--- admin/resources/views/admin/dashboard.blade.php
+++ admin/resources/views/admin/dashboard.blade.php
@@ -7,18 +7,22 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
+    <div class="admin-container">
+        <div class="nav-bar flex ">
+            <div class="nav-content flex row">
+                <div class="title-logo flex row">
+                    <div class="logo">
+                <img src="{{ asset('assets/nsquaredlogo.jpg')}}" alt="img"/>
+                </div>
+                <h1>nsquarred</h1>
+                </div>
+            </div>
+        </div>
+        <script src="{{ asset('js/Chart.min.js') }}"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
+        <script>
 
-    <h1 class='title'>Welcome to the Admin Dashboard</h1>
-
-    <div id="chart-container">
-        <canvas id="myChart"></canvas>
+        </script>
     </div>
-
-    <script src="{{ asset('js/Chart.min.js') }}"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script>
-
-    </script>
-
 </body>
 </html>
